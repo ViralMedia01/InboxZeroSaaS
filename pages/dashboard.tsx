5c701b4da02a5064e91d1503030079592af10f0e
ViralMedia01 <viralmediaonline01@gmail.com>
2025-07-03 14:00:30 +0100

    Add dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,8 @@
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-gray-900">
+      <h1 className="text-4xl font-bold">Welcome to your Dashboard</h1>
+    </div>
+  );
+}
